698cd0244fead8c0083df1d2e16c6d3e46f50256
Sunha-i <icecream20@khu.ac.kr>
2024-05-06 01:11:09 +0900

    Feat: 로그인 페이지 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "../styles/login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div class="container">
+      <div style={{ height: "120px" }}></div>
+      <div class="title">Welcome to 3D box</div>
+      <div style={{ height: "20px" }}></div>
+      <img src="/assets/images/projectdesc.svg" alt="Project Description" />
+    </div>
+  );
 }
