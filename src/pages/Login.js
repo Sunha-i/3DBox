f4f905f959e03eb05f6c98500893af412fae4151
Sunha-i <icecream20@khu.ac.kr>
2024-05-08 04:56:42 +0900

    Design: 디테일 수정 및 사파리 블러 현상 해결

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,11 +16,13 @@ export default function Login() {
 
   return (
     <div class="container">
-      <div style={{ height: "110px" }}></div>
+      <div style={{ height: "120px" }}></div>
       <div class="title">Welcome to 3D box</div>
       <div style={{ height: "20px" }}></div>
-      <img src="/assets/images/projectdesc.svg" alt="Project Description" />
-      <div style={{ height: "50px" }}></div>
+      <object type="image/svg+xml" data="/assets/images/projectdesc.svg">
+          <img src="/assets/images/projectdesc.svg" alt="Project Description" />
+      </object>
+      <div style={{ height: "47px" }}></div>
       <button type="button" onMouseDown={handleButtonPressing} onMouseUp={handleButtonRelease}>
         <img src={ isPressed ? "/assets/images/googlepressing.svg"
                              : "/assets/images/googledefault.svg"} alt="Login with Google" />
