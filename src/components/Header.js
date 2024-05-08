9855fd00423dcf7c757d9230aa2d5a7385df175f
komg00 <komg00@naver.com>
2024-05-08 14:59:03 +0900

    Feat: 메인 페이지 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,5 +2,13 @@ import React from "react";
 import "../styles/header.css";
 
 export default function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <img
+        src="/assets/images/logo.png"
+        alt="logo"
+        style={{ margin: "15px" }}
+      />
+    </div>
+  );
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,5 +2,14 @@ import React from "react";
 import "../styles/sidebar.css";
 
 export default function SideBar() {
-  return <div className="sidebar">SideBar</div>;
+  return (
+    <div className="sidebar">
+      <div className="container clicked">
+        <p>모든 파일</p>
+      </div>
+      <div className="container">
+        <p>즐겨찾기</p>
+      </div>
+    </div>
+  );
 }
