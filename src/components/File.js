46f6c246dc680cd5d43e5f267358de481648609d
komg00 <komg00@naver.com>
2024-05-14 02:19:08 +0900

    Feat: 이름 변경 모달 추가 & 파일 선택했을 때 border 표시

--- src/components/File.js
+++ src/components/File.js
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "../styles/file.module.css";
 import Checkbox from "./CheckBox/Checkbox";
 
-export default function File({ name }) {
+export default function File({ name, checked }) {
   return (
     <div className={styles.file}>
-      <div className={styles.file_container}>
+      <div className={`${styles.file_container} ${checked && styles.checked}`}>
         <Checkbox value={name} />
         <img
           src="/assets/images/cloud.png"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,9 @@ import CheckboxGroup from "../components/CheckBox/CheckboxGroup";
 export default function Home() {
   const [files, setFiles] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
+  const [modifyModalOpen, setModifyModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
   const modalBackground = useRef();
 
   const openModal = () => {
@@ -17,6 +19,14 @@ export default function Home() {
 
   const closeModal = () => {
     setModalOpen(false);
+  };
+
+  const openModifyModal = () => {
+    setModifyModalOpen(true);
+  };
+
+  const closeModifyModal = () => {
+    setModifyModalOpen(false);
   };
 
   const openDeleteModal = () => {
@@ -34,10 +44,11 @@ export default function Home() {
         <SideBar />
         <div className={styles.content}>
           <div className={styles.button_container}>
-            <div className={styles.upload}>
+            <label className={styles.upload}>
+              <input type="file" className={styles.file} />
               <img src="/assets/images/upload.png" alt="upload" />
               <p>업로드</p>
-            </div>
+            </label>
             <div className={styles.create} onClick={openModal}>
               <img src="/assets/images/folder-plus.png" alt="upload" />
               <p>폴더 만들기</p>
@@ -52,7 +63,7 @@ export default function Home() {
               <img src="/assets/images/upload.png" alt="upload" />
               <p style={{ color: "white" }}>다운로드</p>
             </div>
-            <div className={styles.button}>
+            <div className={styles.button} onClick={openModifyModal}>
               <img src="/assets/images/rename.png" alt="upload" />
               <p>이름 변경</p>
             </div>
@@ -112,7 +123,44 @@ export default function Home() {
               </div>
             </div>
           )}
-          {deleteModalOpen && ( // 삭제 모달
+          {modifyModalOpen && ( // 이름 변경 모달
+            <div
+              className={styles.modal}
+              ref={modalBackground}
+              onClick={(e) => {
+                if (e.target === modalBackground.current) {
+                  setModifyModalOpen(false);
+                }
+              }}
+            >
+              <div className={styles.modal_content}>
+                <div className={styles.close}>
+                  <h2>이름 변경</h2>
+                  <img
+                    src="/assets/images/close.png"
+                    alt="close"
+                    onClick={closeModifyModal}
+                    width={30}
+                    height={30}
+                  />
+                </div>
+                <p>이름</p>
+                <input type="text" className={styles.input}></input>
+                <div className={styles.btn_container}>
+                  <button className={styles.btn} onClick={closeModifyModal}>
+                    취소
+                  </button>
+                  <button
+                    className={styles.btn}
+                    style={{ backgroundColor: "#4545C2", color: "white" }}
+                  >
+                    변경
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+          {deleteModalOpen && ( // 파일 & 폴더 삭제 모달
             <div
               className={styles.modal}
               ref={modalBackground}
