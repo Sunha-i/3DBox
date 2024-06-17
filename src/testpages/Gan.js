a8f7f253c91c7095a83cf4ee8cbdabdff8bf99d4
Sunha-i <icecream20@khu.ac.kr>
2024-06-18 00:27:24 +0900

    Design: 윈도우 이미지 변경 및 폴리싱

--- src/testpages/Gan.js
+++ src/testpages/Gan.js
@@ -23,26 +23,30 @@ export default function Gan({ onClose }) {
                                     : "/assets/images/btndefault.svg"} alt="Close Box" />
       </button>
       <div className={styles.ganZone}>
-        <div>
-          <select>
-            <option key="banana" value="banana">Cartoon</option>
-            <option key="apple" value="apple">Illustration</option>
-            <option key="orange" value="orange">Fantasy</option>
-          </select>
-          <select>
-            <option key="banana" value="banana">2013</option>
-            <option key="apple" value="apple">2015</option>
-            <option key="orange" value="orange">2017</option>
-          </select>
-          <img src={ isEmptyBtnPressed ? "/assets/images/emptybtnselected.svg"
-                                        :  "/assets/images/emptybtnnormal.svg"} alt="Empty Button" 
+        <div className={styles.selectZone}>
+          <div>
+            <select>
+              <option key="banana" value="banana">Cartoon</option>
+              <option key="apple" value="apple">Illustration</option>
+              <option key="orange" value="orange">Fantasy</option>
+            </select>
+          </div>
+          <div>
+            <select>
+              <option key="banana" value="banana">2013</option>
+              <option key="apple" value="apple">2015</option>
+              <option key="orange" value="orange">2017</option>
+            </select>
+          </div>
+          <img src={ isEmptyBtnPressed ? "/assets/images/createbtnselected.svg"
+                                        :  "/assets/images/createbtnnormal.svg"} alt="Empty Button" 
                                         onMouseDown={() => { setIsEmptyBtnPressed(true); handleCreate(); }}
                                         onMouseUp={() => {setIsEmptyBtnPressed(false)}} className={styles.btn}/>
         </div>
-        <div>
-          <div className={styles.ganResult}>
+        <div className={styles.ganResult}>
+            <div className={styles.myresult}>
 
-          </div>
+            </div>
         </div>
       </div>
     </div>
