2b24e9fbdd788f5a84cd00aa6eaf475f5452dbb8
Sunha-i <icecream20@khu.ac.kr>
2024-06-18 01:52:19 +0900

    Feat: GAN 결과창 드롭 다운 메뉴, 수정

--- src/testpages/Gan.js
+++ src/testpages/Gan.js
@@ -24,27 +24,35 @@ export default function Gan({ onClose }) {
       </button>
       <div className={styles.ganZone}>
         <div className={styles.selectZone}>
-          <div>
-            <select>
-              <option key="banana" value="banana">Cartoon</option>
-              <option key="apple" value="apple">Illustration</option>
-              <option key="orange" value="orange">Fantasy</option>
-            </select>
-          </div>
-          <div>
-            <select>
+          <div className={styles.selectYear}>
+            <div>Year:</div>
+            <select className={styles.dropDownYear}>
               <option key="banana" value="banana">2013</option>
               <option key="apple" value="apple">2015</option>
               <option key="orange" value="orange">2017</option>
             </select>
           </div>
+          <div className={styles.selectTheme}>
+            <div>Theme:</div>
+            <select className={styles.dropDownTheme}>
+              <option key="banana" value="banana">Cartoon</option>
+              <option key="apple" value="apple">Illustration</option>
+              <option key="orange" value="orange">Fantasy</option>
+            </select>
+          </div>
           <img src={ isEmptyBtnPressed ? "/assets/images/createbtnselected.svg"
-                                        :  "/assets/images/createbtnnormal.svg"} alt="Empty Button" 
-                                        onMouseDown={() => { setIsEmptyBtnPressed(true); handleCreate(); }}
-                                        onMouseUp={() => {setIsEmptyBtnPressed(false)}} className={styles.btn}/>
+                                       : "/assets/images/createbtnnormal.svg"} alt="Empty Button" 
+                                       onMouseDown={() => { setIsEmptyBtnPressed(true); handleCreate(); }}
+                                       onMouseUp={() => {setIsEmptyBtnPressed(false)}} className={styles.btn}/>
         </div>
-        <div className={styles.ganResult}>
-            <div className={styles.myresult}>
+        <div className={styles.divisionLine}>
+          <div style={{ backgroundColor: "#ddd" }} className={styles.line} />
+          <div style={{ backgroundColor: "#bbb" }} className={styles.line} />
+          <div style={{ backgroundColor: "#999" }} className={styles.line} />
+          <div style={{ backgroundColor: "#fff" }} className={styles.line} />
+        </div>
+        <div className={styles.resultZone}>
+            <div className={styles.ganResult}>
 
             </div>
         </div>
